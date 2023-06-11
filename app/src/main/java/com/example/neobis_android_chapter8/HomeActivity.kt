dbea209400fbcc577c458e228a9e72721153f261
rybakova-auca-2021 <rybakova_k@auca.kg>
2023-06-11 23:21:00 +0600

    initial commit

---
+++ app/src/main/java/com/example/neobis_android_chapter8/HomeActivity.kt
@@ -0,0 +1,11 @@
+package com.example.neobis_android_chapter8
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
