9ed6a7aa40585d67aac571767099cf22b6f3562f
rybakova-auca-2021 <rybakova_k@auca.kg>
2023-06-12 23:51:39 +0600

    finished with registration

--- app/src/main/java/com/example/neobis_android_chapter8/view/PasswordRegistration.kt
+++ app/src/main/java/com/example/neobis_android_chapter8/view/PasswordRegistration.kt
@@ -1,12 +1,24 @@
 package com.example.neobis_android_chapter8.view
 
 import android.os.Bundle
+import android.text.method.HideReturnsTransformationMethod
+import android.text.method.PasswordTransformationMethod
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.view.isVisible
+import androidx.core.widget.addTextChangedListener
+import androidx.navigation.fragment.findNavController
 import com.example.neobis_android_chapter8.R
+import com.example.neobis_android_chapter8.Utils
+import com.example.neobis_android_chapter8.api.RetrofitInstance
 import com.example.neobis_android_chapter8.databinding.FragmentPasswordRegistrationBinding
+import com.example.neobis_android_chapter8.model.Register
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class PasswordRegistration : Fragment() {
     private lateinit var binding: FragmentPasswordRegistrationBinding
@@ -14,8 +26,91 @@ class PasswordRegistration : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
     ): View {
-        // Inflate the layout for this fragment
         binding = FragmentPasswordRegistrationBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupNavigation()
+        setupClickListeners()
+        setupPasswordValidation()
+    }
+
+    private fun setupNavigation() {
+        binding.buttonNext.setOnClickListener {
+            register()
+        }
+        binding.buttonBack.setOnClickListener {
+            findNavController().navigate(R.id.action_passwordRegistration_to_registration)
+        }
+    }
+    private fun setupClickListeners() {
+        binding.hidePassword.setOnClickListener {
+            togglePasswordVisibilityCreate()
+        }
+    }
+    private fun setupPasswordValidation() {
+        binding.createPasswordEt.addTextChangedListener {
+            val password = it.toString()
+            var isPasswordMatch = false
+
+            val hasUpperCase = password.matches(".*[A-Z].*".toRegex())
+            val hasNumber = password.matches(".*\\d.*".toRegex())
+            val hasSpecialSymbol = password.matches(".*[!@#$%^&*(),.?\":{}|<>].*".toRegex())
+            if (password == binding.repeatPasswordEt.text.toString()) {
+                isPasswordMatch = true
+            }
+
+            val isValid = hasUpperCase && hasNumber && hasSpecialSymbol && isPasswordMatch
+            if (isValid) {
+                binding.buttonNext.isEnabled = true
+                binding.buttonNext.setBackgroundResource(R.color.btn_valid)
+            }
+        }
+
+        binding.createPasswordEt.addTextChangedListener {
+            val createPassword = it.toString()
+            binding.repeatPasswordEt.isVisible = createPassword.isNotEmpty()
+        }
+    }
+    private fun togglePasswordVisibilityCreate() {
+        val isPasswordVisible = binding.createPasswordEt.transformationMethod == HideReturnsTransformationMethod.getInstance()
+        val newTransformationMethod = if (isPasswordVisible) PasswordTransformationMethod.getInstance() else HideReturnsTransformationMethod.getInstance()
+        binding.createPasswordEt.transformationMethod = newTransformationMethod
+        binding.createPasswordEt.setSelection(binding.createPasswordEt.text?.length ?: 0)
+
+        val isPasswordVisible2 = binding.repeatPasswordEt.transformationMethod == HideReturnsTransformationMethod.getInstance()
+        val newTransformationMethod2 = if (isPasswordVisible2) PasswordTransformationMethod.getInstance() else HideReturnsTransformationMethod.getInstance()
+        binding.repeatPasswordEt.transformationMethod = newTransformationMethod2
+        binding.repeatPasswordEt.setSelection(binding.repeatPasswordEt.text?.length ?: 0)
+    }
+
+    private fun register() {
+        val username = Utils.username
+        val email = Utils.email
+        val password = binding.createPasswordEt.text?.toString() ?: ""
+        val passwordRepeat = binding.repeatPasswordEt.text?.toString() ?: ""
+        val request = Register(username, email, password, passwordRepeat)
+        val apiInterface = RetrofitInstance.api
+
+        val call = apiInterface.register(request)
+        call.enqueue(object : Callback<Unit> {
+            override fun onResponse(
+                call: Call<Unit>,
+                response: Response<Unit>
+            ) {
+                if (response.isSuccessful) {
+                    findNavController().navigate(R.id.action_passwordRegistration_to_profileFragment)
+                    Toast.makeText(requireContext(), "Регистрация прошла успешно", Toast.LENGTH_SHORT).show()
+                } else {
+                    Toast.makeText(requireContext(), "Попробуйте еще раз", Toast.LENGTH_SHORT).show()
+                }
+            }
+
+            override fun onFailure(call: Call<Unit>, t: Throwable) {
+                Toast.makeText(requireContext(), "Повторите попытку", Toast.LENGTH_SHORT).show()
+            }
+        })
     }
 }
--- app/src/main/java/com/example/neobis_android_chapter8/view/Registration.kt
+++ app/src/main/java/com/example/neobis_android_chapter8/view/Registration.kt
@@ -1,11 +1,15 @@
 package com.example.neobis_android_chapter8.view
 
 import android.os.Bundle
+import android.util.Patterns
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.widget.addTextChangedListener
+import androidx.navigation.fragment.findNavController
 import com.example.neobis_android_chapter8.R
+import com.example.neobis_android_chapter8.Utils
 import com.example.neobis_android_chapter8.databinding.FragmentRegistrationBinding
 
 class Registration : Fragment() {
@@ -14,8 +18,47 @@ class Registration : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
-    ): View? {
+    ): View {
        binding = FragmentRegistrationBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupNavigation()
+        changeButtonColor()
+    }
+
+    private fun setupNavigation() {
+        binding.buttonNext.setOnClickListener {
+            getData()
+            findNavController().navigate(R.id.action_registration_to_passwordRegistration)
+        }
+        binding.buttonBack.setOnClickListener {
+            findNavController().navigate(R.id.action_registration_to_login)
+        }
+    }
+
+    private fun getData() {
+        val username = binding.username.text.toString()
+        Utils.username = username
+
+        val email = binding.email.text.toString()
+        Utils.email = email
+    }
+
+    private fun isValidEmail(email: String): Boolean {
+        val pattern = Patterns.EMAIL_ADDRESS
+        return pattern.matcher(email).matches()
+    }
+
+    private fun changeButtonColor() {
+        binding.email.addTextChangedListener { text ->
+            val email = text?.toString() ?: ""
+            val isValidEmail = isValidEmail(email)
+            val buttonColor = if (isValidEmail) R.color.btn_valid else R.color.btn_invalid
+            binding.buttonNext.isEnabled = isValidEmail
+            binding.buttonNext.setBackgroundColor(requireContext().getColor(buttonColor))
+        }
     }
 }
