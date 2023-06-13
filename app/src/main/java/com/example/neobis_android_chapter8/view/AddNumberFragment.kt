8bbd106857fac392a073da83c2034645429a7377
rybakova-auca-2021 <rybakova_k@auca.kg>
2023-06-13 23:39:41 +0600

    created ui for add number page

---
+++ app/src/main/java/com/example/neobis_android_chapter8/view/AddNumberFragment.kt
@@ -0,0 +1,17 @@
+package com.example.neobis_android_chapter8.view
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.neobis_android_chapter8.R
+
+class AddNumberFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View? {
+        return inflater.inflate(R.layout.fragment_add_number, container, false)
+    }
+}
