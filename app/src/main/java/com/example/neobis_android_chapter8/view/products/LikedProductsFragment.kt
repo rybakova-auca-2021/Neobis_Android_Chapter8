12e51307d5426c501da9734042584e11698d04d9
rybakova-auca-2021 <rybakova_k@auca.kg>
2023-06-25 18:32:55 +0600

    added liked products fragment

---
+++ app/src/main/java/com/example/neobis_android_chapter8/view/products/LikedProductsFragment.kt
@@ -0,0 +1,36 @@
+package com.example.neobis_android_chapter8.view.products
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.GridLayoutManager
+import com.example.neobis_android_chapter8.R
+import com.example.neobis_android_chapter8.adapter.RecyclerViewAdapter
+import com.example.neobis_android_chapter8.databinding.FragmentLikedProductsBinding
+
+class LikedProductsFragment : Fragment() {
+    private lateinit var binding: FragmentLikedProductsBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View {
+        binding = FragmentLikedProductsBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupRV()
+    }
+
+    private fun setupRV() {
+        val adapter = RecyclerViewAdapter()
+        val recyclerView = binding.rvLikedProducts
+        recyclerView.adapter = adapter
+        recyclerView.setHasFixedSize(true)
+        recyclerView.layoutManager = GridLayoutManager(requireContext(), 2)
+    }
+}
